98fef06377cd188d07230bf0253674b385068416
Vincent Chabot <1871061@cstj.qc.ca>
2020-05-14 14:48:23 -0400

    init

---
+++ js/eventAnimationScript.js
@@ -0,0 +1,14 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Gestion des évènements d'interaction
+ * 
+ */
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Permet d'animer (ouverture et fermeture) le panneau du 
+ * menu mobile
+ * 
+ */
+
+
+
